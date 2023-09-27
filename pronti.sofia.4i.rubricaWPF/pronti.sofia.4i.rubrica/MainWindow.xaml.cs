7b8d273f9a7cd9fcbd38fc38ee390b7644122c0c
Sofia <127590382+prontisofia@users.noreply.github.com>
2023-09-27 21:37:28 +0200

    Add files via upload

---
+++ pronti.sofia.4i.rubricaWPF/pronti.sofia.4i.rubrica/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pronti.sofia._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Contatto c = new Contatto();
+            c.Numero = 1;
+            c.Nome = "Sofia";
+            c.Cognome = "Pronti";
+
+            Contatto[] Contatti = new Contatto[100];
+            Contatti[0] = c;
+
+            Contatti[0].Nome = "Riccardo";
+            Contatti[0].Cognome = "Bianchi";
+        }
+    }
+}
